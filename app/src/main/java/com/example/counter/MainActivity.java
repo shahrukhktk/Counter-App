1f10cf30b7b9197c60a72910916e13cd9225665b
shahrukhktk <shahrukhktk760@gmail.com>
2019-07-03 13:14:02 +0500

    Counter Source Code

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void Count(View view)
     {
-        Counter = Counter ++;
+        Counter ++;
         textView.setText(""+Counter);
     }
 
